3c3c2004c5ed979f6edc7d65e461fec79a12d24c
Camilatrentino <camila.f.trentino@gmail.com>
2024-02-22 11:05:09 -0300

    aula- 22/02

--- aula-2 exercício lampada/js/script.js
+++ aula-2 exercício lampada/js/script.js
@@ -2,6 +2,7 @@ const ligar = document.getElementById('ligar')
 const desligar = document.getElementById('desligar')
 const lampada = document.getElementById('lamp')
 const rest = document.getElementById('rest')
+const int = document.getElementById('int')
 
 function estaquebrada(){
     return lampada.src.indexOf('quebrada') > -1
@@ -28,6 +29,8 @@ function restaurar(){
     lampada.src = 'img/desligada.jpg'
 }
 
+int.addEventListener('click',lampligada)
+int.addEventListener('dblclick',lampdesligada)
 ligar.addEventListener('click',lampligada)
 desligar.addEventListener('click',lampdesligada)
 
