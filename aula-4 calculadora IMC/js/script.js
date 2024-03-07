596aeb1d097967971f89fe36501e3600a95fd284
Camilatrentino <Camila.f.trentino@gmail.com>
2024-03-07 11:47:16 -0300

    dia 07/03

---
+++ aula-4 calculadora IMC/js/script.js
@@ -0,0 +1,20 @@
+var altu = document.getElementById('altu')
+var peso = document.getElementById('peso')
+var sex = document.getElementById('sexo')
+
+// function calcular(){
+//     var altu = Number(document.getElementById('altu').value);
+//     var altun = altu.replace(",", ".");
+//     altun = parseFloat(altun);
+//     console.log(altun)
+//     var peso = Number(document.getElementById('peso')
+
+//     var result = peso/altun * altun;
+//     console.log(result)
+
+// }
+
+function limpar(){
+    document.getElementById('altu').value= " "
+    document.getElementById('peso').value= " "
+}
