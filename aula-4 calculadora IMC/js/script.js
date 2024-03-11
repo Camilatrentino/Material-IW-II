ac49c629defeb306fee0f56422636f8eb973a5a9
projetoscintia <cpinho@tecnologia-unica.com.br>
2024-03-11 12:03:46 -0300

    arrumei css

--- aula-4 calculadora IMC/js/script.js
+++ aula-4 calculadora IMC/js/script.js
@@ -1,20 +1,61 @@
-var altu = document.getElementById('altu')
+var altura = document.getElementById('altura')
 var peso = document.getElementById('peso')
-var sex = document.getElementById('sexo')
+var resultado = document.getElementById('resultado')
+var diag = document.getElementById('diag')
+var sexo = document.getElementById("sexo").value
 
-// function calcular(){
-//     var altu = Number(document.getElementById('altu').value);
-//     var altun = altu.replace(",", ".");
-//     altun = parseFloat(altun);
-//     console.log(altun)
-//     var peso = Number(document.getElementById('peso')
+function calculo(){
+    var altura = Number(document.getElementById('altura').value)
+    var peso = Number(document.getElementById('peso').value)
+    var conta = peso/(altura^2)
+    resultado.innerHTML = conta
+    diag.innerHTML = classIMC(sexo, conta)
+    console.log('Resultado da operação é: ', conta)
+}
 
-//     var result = peso/altun * altun;
-//     console.log(result)
+function limp(){
+    resultado.innerHTML = '0'
+    diag.innerHTML = ' '
+    document.getElementById('altura').value = ''
+    document.getElementById('peso').value = ''
 
-// }
+}
 
-function limpar(){
-    document.getElementById('altu').value= " "
-    document.getElementById('peso').value= " "
+function classIMC(sexo, conta){
+    if(sexo = "masculino"){
+        return classmasc(conta)
+    }
+    else{
+        return classificarFeminino(conta)
+    }
+}
+
+function classmasc(conta){
+    if(conta<18.5){
+        return "Você está abaixo do peso indicado"
+    }
+    else if(conta >= 18.5 && conta < 24.9){
+    return "Você está com o peso indicado"
+    }
+    else if(conta >= 25 && conta < 29.9){
+    return "Você está acima do peso indicado"
+    }
+    else if(conta > 30){
+    return "Você está muito acima do peso indicado"
+    }
+}
+
+function classfem(conta){
+    if(conta<18.5){
+        return "Você está abaixo do peso indicado"
+    }
+    else if(conta >= 18.5 && conta < 24.9){
+    return "Você está com o peso indicado"
+    }
+    else if(conta >= 25 && conta < 29.9){
+    return "Você está acima do peso indicado"
+    }
+    else if(conta > 30){
+    return "Você está muito acima do peso indicado"
+    }
 }
